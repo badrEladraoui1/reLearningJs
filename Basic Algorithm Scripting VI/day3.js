05445ab091e53c63ae96812cdece5c51e8d579da
badrEladraoui1 <eladraoui.badr@gmail.com>
2024-01-12 20:10:10 +0100

    solving prblms

--- Basic Algorithm Scripting VI/day3.js
+++ Basic Algorithm Scripting VI/day3.js
@@ -16,6 +16,9 @@
 
 
 
+
+
+
 // ! Falsy Bouncer
 
 {
@@ -39,4 +42,75 @@
     console.log(bouncer([7, "ate", "", false, 9]))
 }
 
+// * or : 
+
+{
+    function bouncer(arr) {
+        return arr.filter(Boolean);
+    }
+    console.log(bouncer([7, "ate", "", false, 9]))
+}
+
+// * or : 
+
+{
+    function bouncer(arr) {
+        const filteredArr = [];
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i]) filteredArr.push(arr[i]);
+        }
+        return filteredArr;
+    }
+}
+
 // ! Where do I Belong
+
+// * my Solution 
+
+{
+    function getIndexToIns(arr, num) {
+
+        let index
+
+        const newArr = [...arr]
+        newArr.push(num)
+        const sorted = newArr.sort((a, b) => a - b)
+        index = sorted.indexOf(num)
+        return index;
+
+    }
+
+    console.log(getIndexToIns([2, 5, 10], 15))
+}
+
+// * chat gpt's : 
+
+{
+    function getIndexToIns(arr, num) {
+        // Insert num into arr at the correct position
+        arr.push(num);
+        arr.sort((a, b) => a - b);
+
+        // Find the index of num in the sorted array
+        const index = arr.indexOf(num);
+
+        return index;
+    }
+
+    console.log(getIndexToIns([2, 5, 10], 15));
+}
+
+// * best one : 
+
+{
+    function getIndexToIns(arr, num) {
+        const sortedIndex = arr.findIndex(element => element >= num);
+        return sortedIndex === -1 ? arr.length : sortedIndex;
+    }
+
+    console.log(getIndexToIns([2, 5, 10], 15)); // Output: 3
+
+    // ? This version uses findIndex to locate the index where element is greater than or equal to num. If no such element is found,
+    // ? it returns arr.length, indicating that num should be inserted at the end of the array.
+    // ? This approach avoids sorting the entire array, making it more efficient for larger arrays.
+}
