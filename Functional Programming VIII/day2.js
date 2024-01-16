b6405a61ea3138b69f415125d667ab07cd3e5142
badrEladraoui1 <eladraoui.badr@gmail.com>
2024-01-16 18:45:39 +0100

    functional programing

--- Functional Programming VIII/day2.js
+++ Functional Programming VIII/day2.js
@@ -132,3 +132,278 @@ console.log(fixedValue)
 // Adding one to a number is not very exciting, but we can apply these principles when working with arrays or more complex objects.
 
 
+// The global variable
+const bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
+
+
+{
+    function add(bookList, bookName) {
+        return [...bookList, bookName]
+    }
+
+
+    function remove(bookList, bookName) {
+        const book_index = bookList.indexOf(bookName);
+        if (book_index >= 0) {
+            return bookList.slice(0, book_index).concat(bookList.slice(book_index + 1));
+            return [...bookList.slice(0, book_index), ...bookList.slice(book_index + 1)] // ?  we can do this too
+        }
+    }
+
+    console.log("original bookList at first : ", bookList)
+
+    console.log("Adding KITAB : ", add(bookList, 'KITAB'))
+    console.log("original bookList after adding : ", bookList)
+
+    console.log("removing The Hound of the Baskervilles : ", remove(bookList, 'The Hound of the Baskervilles'))
+    console.log("original bookList after removing : ", bookList)
+
+    console.log("remove add : ", remove(add(bookList, "A Brief History of Time"), "On The Electrodynamics of Moving Bodies"))
+    console.log("original bookList after remove add : ", bookList)
+}
+
+
+
+
+
+
+
+
+// ! Use the map Method to Extract Data from an Array : .map()
+
+// * This is only the beginning.As its name suggests, functional programming is centered around a theory of functions.
+
+// * It would make sense to be able to pass them as arguments to other functions, and return a function from another function.
+// * Functions are considered first class objects in JavaScript, which means they can be used like any other object
+// * They can be saved in variables, stored in an object, or passed as function arguments.
+
+// * Let's start with some simple array functions, which are methods on the array object prototype.
+// * In this exercise we are looking at Array.prototype.map(), or more simply map.
+
+// * The map method iterates over each item in an array and returns a new array containing the results of calling the callback function on each element.
+// * It does this without mutating the original array.
+
+// ? When the callback is used, it is passed three arguments. The first argument is the current element being processed.
+// ? The second is the index of that element and the third is the array upon which the map method was called.
+
+{
+    const users = [
+        { name: 'John', age: 34 },
+        { name: 'Amy', age: 20 },
+        { name: 'camperCat', age: 10 }
+    ];
+
+    const names = users.map(user => user.name);
+    console.log(names); // ? [ 'John', 'Amy', 'camperCat' ]
+}
+
+
+
+{// The global variable
+    const watchList = [
+        {
+            "Title": "Inception",
+            "Year": "2010",
+            "Rated": "PG-13",
+            "Released": "16 Jul 2010",
+            "Runtime": "148 min",
+            "Genre": "Action, Adventure, Crime",
+            "Director": "Christopher Nolan",
+            "Writer": "Christopher Nolan",
+            "Actors": "Leonardo DiCaprio, Joseph Gordon-Levitt, Elliot Page, Tom Hardy",
+            "Plot": "A thief, who steals corporate secrets through use of dream-sharing technology, is given the inverse task of planting an idea into the mind of a CEO.",
+            "Language": "English, Japanese, French",
+            "Country": "USA, UK",
+            "Awards": "Won 4 Oscars. Another 143 wins & 198 nominations.",
+            "Poster": "http://ia.media-imdb.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+            "Metascore": "74",
+            "imdbRating": "8.8",
+            "imdbVotes": "1,446,708",
+            "imdbID": "tt1375666",
+            "Type": "movie",
+            "Response": "True"
+        },
+        {
+            "Title": "Interstellar",
+            "Year": "2014",
+            "Rated": "PG-13",
+            "Released": "07 Nov 2014",
+            "Runtime": "169 min",
+            "Genre": "Adventure, Drama, Sci-Fi",
+            "Director": "Christopher Nolan",
+            "Writer": "Jonathan Nolan, Christopher Nolan",
+            "Actors": "Ellen Burstyn, Matthew McConaughey, Mackenzie Foy, John Lithgow",
+            "Plot": "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+            "Language": "English",
+            "Country": "USA, UK",
+            "Awards": "Won 1 Oscar. Another 39 wins & 132 nominations.",
+            "Poster": "http://ia.media-imdb.com/images/M/MV5BMjIxNTU4MzY4MF5BMl5BanBnXkFtZTgwMzM4ODI3MjE@._V1_SX300.jpg",
+            "Metascore": "74",
+            "imdbRating": "8.6",
+            "imdbVotes": "910,366",
+            "imdbID": "tt0816692",
+            "Type": "movie",
+            "Response": "True"
+        },
+        {
+            "Title": "The Dark Knight",
+            "Year": "2008",
+            "Rated": "PG-13",
+            "Released": "18 Jul 2008",
+            "Runtime": "152 min",
+            "Genre": "Action, Adventure, Crime",
+            "Director": "Christopher Nolan",
+            "Writer": "Jonathan Nolan (screenplay), Christopher Nolan (screenplay), Christopher Nolan (story), David S. Goyer (story), Bob Kane (characters)",
+            "Actors": "Christian Bale, Heath Ledger, Aaron Eckhart, Michael Caine",
+            "Plot": "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice.",
+            "Language": "English, Mandarin",
+            "Country": "USA, UK",
+            "Awards": "Won 2 Oscars. Another 146 wins & 142 nominations.",
+            "Poster": "http://ia.media-imdb.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg",
+            "Metascore": "82",
+            "imdbRating": "9.0",
+            "imdbVotes": "1,652,832",
+            "imdbID": "tt0468569",
+            "Type": "movie",
+            "Response": "True"
+        },
+        {
+            "Title": "Batman Begins",
+            "Year": "2005",
+            "Rated": "PG-13",
+            "Released": "15 Jun 2005",
+            "Runtime": "140 min",
+            "Genre": "Action, Adventure",
+            "Director": "Christopher Nolan",
+            "Writer": "Bob Kane (characters), David S. Goyer (story), Christopher Nolan (screenplay), David S. Goyer (screenplay)",
+            "Actors": "Christian Bale, Michael Caine, Liam Neeson, Katie Holmes",
+            "Plot": "After training with his mentor, Batman begins his fight to free crime-ridden Gotham City from the corruption that Scarecrow and the League of Shadows have cast upon it.",
+            "Language": "English, Urdu, Mandarin",
+            "Country": "USA, UK",
+            "Awards": "Nominated for 1 Oscar. Another 15 wins & 66 nominations.",
+            "Poster": "http://ia.media-imdb.com/images/M/MV5BNTM3OTc0MzM2OV5BMl5BanBnXkFtZTYwNzUwMTI3._V1_SX300.jpg",
+            "Metascore": "70",
+            "imdbRating": "8.3",
+            "imdbVotes": "972,584",
+            "imdbID": "tt0372784",
+            "Type": "movie",
+            "Response": "True"
+        },
+        {
+            "Title": "Avatar",
+            "Year": "2009",
+            "Rated": "PG-13",
+            "Released": "18 Dec 2009",
+            "Runtime": "162 min",
+            "Genre": "Action, Adventure, Fantasy",
+            "Director": "James Cameron",
+            "Writer": "James Cameron",
+            "Actors": "Sam Worthington, Zoe Saldana, Sigourney Weaver, Stephen Lang",
+            "Plot": "A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
+            "Language": "English, Spanish",
+            "Country": "USA, UK",
+            "Awards": "Won 3 Oscars. Another 80 wins & 121 nominations.",
+            "Poster": "http://ia.media-imdb.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_SX300.jpg",
+            "Metascore": "83",
+            "imdbRating": "7.9",
+            "imdbVotes": "876,575",
+            "imdbID": "tt0499549",
+            "Type": "movie",
+            "Response": "True"
+        }
+    ];
+
+    // Only change code below this line
+
+    // const ratings = [];
+    // for (let i = 0; i < watchList.length; i++) {
+    //   ratings.push({title: watchList[i]["Title"], rating: watchList[i]["imdbRating"]});
+    // }
+
+    const ratings = watchList.map(item => {
+        return {
+            title: item.Title,
+            rating: item.imdbRating,
+        }
+    })
+
+    // Only change code above this line
+
+    console.log(JSON.stringify(ratings));
+}
+
+
+
+// The for...in loop in JavaScript is used for iterating over the enumerable properties of an object. It is not specifically designed for iterating over the elements of an array, although it can be used for that purpose. Here's the basic syntax of the for...in loop:
+
+{
+    for (variable in object) {
+        // code to be executed
+    }
+}
+
+// variable: A variable that will be assigned the property name (or index in the case of an array) on each iteration.
+// object: The object (or array) whose enumerable properties you want to iterate over.
+
+
+// ! Implement map on a Prototype
+
+// * Method 1 : for loop
+{
+    Array.prototype.myMap = function (callback) {
+        const newArray = [];
+        // Only change code below this line
+        for (let i = 0; i < this.length; i++) {
+            newArray.push(callback(this[i], i, this))
+        };
+        return newArray
+    }
+
+    const result = [23, 65, 98, 5, 13].myMap(item => item * 2)
+    console.log(result)
+    // [46, 130, 196, 10, 26]
+}
+
+// * Method 2 : forEach loop the callback function can take up to three parameters currentValue , index (optional) , array (optional)
+{
+    Array.prototype.myMap = function (callback) {
+        const newArray = [];
+        // Only change code below this line
+        this.forEach((element, index, originalArr) =>
+            newArray.push(callback(element, index, originalArr))
+        );
+        return newArray
+    }
+    const result = [23, 65, 98, 5, 13].myMap(item => item * 2)
+    console.log(result)
+    // [46, 130, 196, 10, 26]
+}
+
+
+
+
+
+
+
+// ! Use the filter Method to Extract Data from an Array : .filter()
+
+// * Another useful array function is Array.prototype.filter(), or simply filter().
+
+// * filter calls a function on each element of an array and returns a new array containing only the elements for which that function
+// * returns a truthy value - that is, a value which returns true if passed to the Boolean() constructor. In other words, it filters
+// * the array, based on the function passed to it. Like map, it does this without needing to modify the original array.
+
+// * The callback function accepts three arguments. The first argument is the current element being processed.
+// * The second is the index of that element and the third is the array upon which the filter method was called.
+
+{
+    const users = [
+        { name: 'John', age: 34 },
+        { name: 'Amy', age: 20 },
+        { name: 'camperCat', age: 10 }
+    ];
+
+    const usersUnder30 = users.filter(user => user.age < 30);
+    console.log(usersUnder30); // ? [ { name: 'Amy', age: 20 }, { name: 'camperCat', age: 10 } ]
+}
+
